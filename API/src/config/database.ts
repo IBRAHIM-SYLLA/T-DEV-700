1844712b413ffd6ee161f995ae7793c3e0e9fc2c
ibrahim-sylla <91750360+IBRAHIM-SYLLA@users.noreply.github.com>
2025-10-15 11:08:00 +0200

    script: database

--- API/src/config/database.ts
+++ API/src/config/database.ts
@@ -6,7 +6,7 @@ dotenvConfig({ path: path.resolve(process.cwd(), ".env") });
 
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3307,
+    port: Number(process.env.DB_PORT) || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD || "",
     database: process.env.DB_NAME,
