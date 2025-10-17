0d5a37e0eca956113954405325f1724879d1c4e0
Marie-Anne Roth <marie-anne.roth@epitech.eu>
2025-10-17 12:11:23 +0200

    QuickFix: add variables for PMA and Vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   ],
     server: {
     host: true,      
-    port: 3000,      
+    port: parseInt(process.env.FRONTEND_PORT),      
     strictPort: true,
     watch: {
       usePolling: true, 
