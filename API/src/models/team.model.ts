8090024c33a4ba7c81a5fe075555b7039d822e37
ibrahim-sylla <91750360+IBRAHIM-SYLLA@users.noreply.github.com>
2025-12-07 18:09:46 +0100

    TypeORM in progress

--- API/src/models/team.model.ts
+++ API/src/models/team.model.ts
@@ -1,10 +1,23 @@
+import { PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany, Entity } from "typeorm";
 import { UserModel } from "./user.model";
 
+@Entity("teams")
 export class TeamModel {
+    @PrimaryGeneratedColumn()
     team_id!: number;
+
+    @Column()
     name!: string;
+
+    @Column({ nullable: true })
     description!: string;
-    manager_id!: number;
-    manager!: UserModel;
+
+    // Relation vers le manager (un user)
+    @ManyToOne(() => UserModel, { nullable: true })
+    @JoinColumn({ name: "manager_id" })
+    manager!: UserModel | null;
+
+    // Relation inverse users → team
+    @OneToMany(() => UserModel, user => user.team)
     members!: UserModel[];
 }
