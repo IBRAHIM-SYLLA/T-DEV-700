4e3aabb72ac90d83b228aa4276657c611e0d5aa1
ismail <83200379+ismail3080@users.noreply.github.com>
2025-12-15 10:41:57 +0100

    Change DataService import to .js extension

--- Frontend/src/pages/employee/Historique.jsx
+++ Frontend/src/pages/employee/Historique.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "../../style/style.ts";
-import DataService from "../../services/DataService";
+
+import DataService from "../../services/DataService.js";
 import AttendanceService from "../../services/AttendanceService";
 
 export default function Historique({ timeData }) {
