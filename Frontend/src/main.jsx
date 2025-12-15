57e1d52b4dae32b2ba1cfe5d3d492d4cea33d1af
ismail3080 <ismail.lichir@epitech.eu>
2025-12-15 10:20:35 +0100

    Front View updated

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,12 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
