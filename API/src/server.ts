5b2c9c5e309821ee9c9c6bb7176ca9399b7b9530
Marie-Anne Roth <marie-anne.roth@epitech.eu>
2026-01-06 11:43:36 +0100

    fix(api): add express import and fix TypeScript config

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,22 +1,32 @@
 import dotenv from "dotenv";
+import express from "express";
 import cors from 'cors';
 import path from "path";
 import { testConnection } from './config/database';
 import UserRouter from './routes/UserRouter';
 import AuthRouter from './routes/AuthRouter';
 
-dotenv.config();
-//création de l'app express
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/users', UserRouter);
-app.use('/api/auth', AuthRouter);
-
 const envPath = path.resolve(process.cwd(), '.env');
 dotenv.config({ path: envPath });
 
+//création de l'app express
+const app = express();
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+
+//Routes
+app.use('/api/users', UserRouter);
+app.use('/api/auth', AuthRouter);
+
+// dotenv.config();
+
+
+// route de test
+app.get('/', (req, res) => {
+  res.send('✅ Time Manager API is running');
+});
 
 if (process.env.NODE_ENV !== 'test') {
   const PORT = process.env.BACKEND_PORT || 5001;
@@ -26,17 +36,11 @@ if (process.env.NODE_ENV !== 'test') {
   });
 }
 
-// route de test
-app.get('/', (req, res) => {
-  res.send('✅ Time Manager API is running');
-});
-
 // Démarrage du serveur
-const PORT = process.env.BACKEND_PORT || 5001;
-app.listen(process.env.BACKEND_PORT || 5001, async () => {
-  console.log(`🚀 Server running on port ${process.env.BACKEND_PORT || 5001}, http://localhost:${PORT}`);
-  await testConnection();
-});
+// const PORT = process.env.BACKEND_PORT || 5001;
+// app.listen(process.env.BACKEND_PORT || 5001, async () => {
+//   console.log(`🚀 Server running on port ${process.env.BACKEND_PORT || 5001}, http://localhost:${PORT}`);
+//   await testConnection();
+// });
 
 export default app;
-
