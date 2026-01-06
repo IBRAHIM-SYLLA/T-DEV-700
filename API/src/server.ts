5d6e909067290e19f7d773b1ef0cc73ce52b11f7
ibrahim-sylla <91750360+IBRAHIM-SYLLA@users.noreply.github.com>
2026-01-06 14:44:31 +0100

    feat(): post clock and get user clock ok swagger doc in progress

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,42 +1,26 @@
 import dotenv from "dotenv";
-import cors from 'cors';
 import path from "path";
-import { testConnection } from './config/database';
-import UserRouter from './routes/UserRouter';
-import AuthRouter from './routes/AuthRouter';
+import { AppDataSource, testConnection } from './config/database';
+import app from "./index";
 
 dotenv.config();
-//création de l'app express
-const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.use('/api/users', UserRouter);
-app.use('/api/auth', AuthRouter);
+console.log("🔥 SERVER.TS EXECUTÉ");
 
 const envPath = path.resolve(process.cwd(), '.env');
 dotenv.config({ path: envPath });
-
-
-if (process.env.NODE_ENV !== 'test') {
-  const PORT = process.env.BACKEND_PORT || 5001;
-  app.listen(PORT, async () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-    await testConnection();
-  });
-}
-
-// route de test
-app.get('/', (req, res) => {
-  res.send('✅ Time Manager API is running');
-});
-
 // Démarrage du serveur
 const PORT = process.env.BACKEND_PORT || 5001;
-app.listen(process.env.BACKEND_PORT || 5001, async () => {
-  console.log(`🚀 Server running on port ${process.env.BACKEND_PORT || 5001}, http://localhost:${PORT}`);
-  await testConnection();
-});
 
-export default app;
 
+AppDataSource.initialize()
+  .then(() => {
+    console.log("✅ DataSource initialisé");
+    app.listen(process.env.BACKEND_PORT || 5001, async () => {
+      console.log(`🚀 Server running on port ${process.env.BACKEND_PORT || 5001}, http://localhost:${PORT}`);
+      await testConnection();
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Erreur TypeORM :", err);
+  });
