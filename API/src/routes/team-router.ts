94a7aa30c470489db7e968f02ce774c387e826d3
ibrahim-sylla <91750360+IBRAHIM-SYLLA@users.noreply.github.com>
2026-01-09 15:12:20 +0100

    fix(): teams router

--- API/src/routes/team-router.ts
+++ API/src/routes/team-router.ts
@@ -1,41 +1,20 @@
 import express, { Router, Request, Response } from "express";
 import { TeamService } from "../services/team-service";
 import { verifyAdminRh, verifyManager, verifyToken } from "../utils/UserMiddleware";
+import { log } from "console";
 
 const teamRouter: Router = express.Router();
 
 const teamService: TeamService = new TeamService();
 
 /**
- * @route POST /teams/
- * @desc Creer un utilisateur
- */
-teamRouter.get("/", verifyToken, verifyAdminRh, async (req: Request, res: Response) => {
-    try {
-        const teams = await teamService.getAllTeams();
-        res.status(200).json(teams);
-    } catch (error) {
-        console.error("Erreur lors de la récupération des équipes :", error);
-        res.status(500).json({ message: "Erreur interne du serveur" });
-    }
-})
-
-teamRouter.get("/:id", verifyToken, verifyAdminRh, async (req: Request, res: Response) => {
-    try {
-        const team = await teamService.getTeamById(Number.parseInt(req.params.id));
-        res.status(200).json(team);
-    } catch (error) {
-        console.error("Erreur lors de la récupération de l'équipes :", error);
-        res.status(500).json({ message: "Erreur interne du serveur" });
-    }
-})
-
-/**
  * GET /teams/manager/:managerId
  * Récupère toutes les équipes d'un manager avec leurs membres
  */
-teamRouter.get("/manageTeams/", verifyToken, verifyManager, async (req: Request, res: Response) => {
+teamRouter.get("/manageTeams", verifyToken, verifyManager, async (req: Request, res: Response) => {
     try {
+        console.log("test eret");
+
         const managerId = req.user!.user_id;
 
         if (isNaN(managerId)) {
@@ -50,6 +29,33 @@ teamRouter.get("/manageTeams/", verifyToken, verifyManager, async (req: Request,
         return res.status(500).json({ message: "Erreur serveur" });
     }
 });
+
+
+/**
+ * @route POST /teams/
+ * @desc Creer un utilisateur
+ */
+teamRouter.get("/", /*verifyToken, verifyAdminRh,*/ async (req: Request, res: Response) => {
+    try {
+        const teams = await teamService.getAllTeams();
+        res.status(200).json(teams);
+    } catch (error) {
+        console.error("Erreur lors de la récupération des équipes :", error);
+        res.status(500).json({ message: "Erreur interne du serveur" });
+    }
+})
+
+teamRouter.get("/:id", verifyToken, verifyAdminRh, async (req: Request, res: Response) => {
+    try {
+        console.log("test eret");
+        const team = await teamService.getTeamById(Number.parseInt(req.params.id));
+        res.status(200).json(team);
+    } catch (error) {
+        console.error("Erreur lors de la récupération de l'équipes :", error);
+        res.status(500).json({ message: "Erreur interne du serveur" });
+    }
+})
+
 
 teamRouter.post("/", verifyToken, verifyAdminRh, async (req: Request, res: Response) => {
     try {
--- API/src/utils/UserMiddleware.ts
+++ API/src/utils/UserMiddleware.ts
@@ -17,6 +17,7 @@ export const verifyToken = (
   }
 
   const token = authHeader.split(" ")[1];
+  console.log(token)
 
   jwt.verify(token, process.env.JWT_SECRET!, (err, decoded) => {
     if (err) {
